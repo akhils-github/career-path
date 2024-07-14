345313ae6b0a9435510043133322ec041e13b20e
akhils <akhil@webyfy.com>
2024-07-14 21:59:35 +0530

    updated

--- src/components/profile/ProfileHeader.jsx
+++ src/components/profile/ProfileHeader.jsx
@@ -4,13 +4,19 @@ import { Link } from "react-router-dom";
 // import Tick from "/images/profile/tick-circle.svg";
 export default function ProfileHeader() {
   return (
-    <div className="z-[99999] ">
+    <div className="w-full">
       <div className="-mt-12 overflow-y-min mx-auto rounded ">
         {/* search */}
         <div className="text-[#2c2c2c] rounded-md px-5 bg-white gap-4 flex pt-5 pb-5 h-44  shadow">
           <div className="-mt-12 relative p-3 bg-white w-40  rounded shadow-md">
-            <img className=" cursor-pointer w-full h-full object-contain rounded" src="/images/tony.png"  />
-            <label htmlFor="upload" className="bg-[#275DF5] cursor-pointer rounded-full text-white size-10 flex items-center justify-center absolute right-0 -bottom-0">
+            <img
+              className=" cursor-pointer w-full h-full object-contain rounded"
+              src="/images/tony.png"
+            />
+            <label
+              htmlFor="upload"
+              className="bg-[#275DF5] cursor-pointer rounded-full text-white size-10 flex items-center justify-center absolute right-0 -bottom-0"
+            >
               <Camera />
             </label>
             <input type="file" hidden className="hidden" id="upload" />
